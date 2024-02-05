b069a35dc472a64dfe6651093a6593903408ca10
LucasThTrT <thietartl@gmail.com>
2024-02-05 01:31:29 +0100

    fitst commit

---
+++ submit.php
@@ -0,0 +1,27 @@
+<?php    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['name'];
+        $visitor_email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        
+        $email_from = 'lucasthietart@hotmail.com';
+        
+        $email_subject = 'Nouveau Formulaire Rempli !!';
+        
+        $email_body = "Nom: $name.\n".
+                        "Email: $visitor_email.\n".
+                        "Sujet: $subject.\n".
+                        "Message: $message .\n";
+        
+        $to = 'thietartl@gmail.com';
+        
+        $headers = "From: $email_from \r\n";
+        
+        $headers .= "Reply-To: $visitor_email \r\n";
+        
+        mail($to,$email_subject,$email_body,$headers);
+        
+        echo "<p>Votre Formulaire a bien été transmis</p>";
+    }  
+?>
