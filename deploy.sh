cc101761d7dc017b952418b2a80673dd556b4243
Lucas THIETART <thietartl@gmail.com>
2024-03-18 00:28:14 +0100

    automatisation deploiement du site

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Chemin vers le répertoire de votre site web
+WEB_DIR=~/n7racing
+
+# Aller dans le répertoire du site web
+cd $WEB_DIR || exit
+
+# Mettre à jour le dépôt Git
+git pull origin master
